044ae80b5b8e33540f6a923d25d7084d8a8ea71f
unknown <3224387705@qq.com>
2026-06-07 14:26:06 +0800

    Docs: 更新，斩获84.10%真实准确率，并同步补充本地运行须知

--- dataset.py
+++ dataset.py
@@ -1,28 +1,55 @@
+import os
 import torch
 from torch.utils.data import Dataset
 from transformers import BertTokenizer
 
 class testDataset(Dataset):
-    def __init__(self, texts, labels, max_len=64):
-        self.labels = labels
-        print(f"texts的类型是: {type(texts)}，总长度是: {len(texts)}")
-        print(f"labels的总长度是: {len(labels)}")
+    def __init__(self, file_path, max_len):
         self.tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
-        clean_texts = [str(t) for t in texts]
-        self.encodings = self.tokenizer(
-            list(texts),
-            max_length=max_len,
-            padding='max_length',
-            truncation=True,
-            return_tensors="pt"
-        )
-        print(f"input_ids矩阵形状为: {self.encodings['input_ids'].shape}")
+        self.max_len = max_len
+        self.texts = []
+        self.labels = []
+        labels_raw = []
+
+        with open(file_path, "r", encoding="utf-8") as f:
+            for line in f:
+                line = line.strip()
+                if not line:
+                    continue
+                parts = line.split("_!_")
+                if len(parts) < 4:
+                    continue
+                self.texts.append(parts[3])
+                labels_raw.append(parts[1])
+
+        unique_labels = sorted(list(set(labels_raw)))
+        label_map = {raw_id: idx for idx, raw_id in enumerate(unique_labels)}
+
+        self.labels = [label_map[raw_id] for raw_id in labels_raw]
+
+        self.num_classes = len(unique_labels)
 
     def __len__(self):
-        return len(self.labels)
-    def __getitem__(self, idx):
+        return len(self.texts)
+
+    def __getitem__(self, index):
+        text = str(self.texts[index])
+        label = self.labels[index]
+
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            return_token_type_ids=False,
+            padding='max_length',
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors='pt',
+        )
+
         return {
-            'input_ids': self.encodings['input_ids'][idx],
-            'attention_mask': self.encodings['attention_mask'][idx],
-            'label': torch.tensor(self.labels[idx], dtype=torch.long)
+            'text': text,
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'label': torch.tensor(label, dtype=torch.long)
         }
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 import torch
 import swanlab
 from torch.utils.data import DataLoader
-from openai import OpenAI
 from transformers import BertTokenizer
 
 from model import berttestClassifier
@@ -14,9 +13,7 @@ with open("config.json", "r", encoding="utf-8") as f:
     config = json.load(f)
 
 def main():
-    deepseek_key = os.environ.get("DEEPSEEK_API_KEY")
     my_key = config.get("swanlab_api_key")
-
     if my_key:
         swanlab.login(api_key=my_key)
         print("SwanLab已登录")
@@ -24,73 +21,25 @@ def main():
         raise ValueError(
             "\n报错：config.json找不到名叫'swanlab_api_key'\n"
         )
-    if deepseek_key:
-        client = OpenAI(api_key=deepseek_key, base_url="https://api.api-deepseek.com")
 
     swanlab.init(
         project="Headline_Project_1",
         experiment_name=f"bert_lr{config['lr']}_batch{config['train_batch_size']}",
         config=config
     )
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
-
-
-    train_texts = []
-    train_labels_raw = []
-    train_file = os.path.join(config['data_dir'], "train_3k.txt")
-    with open(train_file, "r", encoding="utf-8") as f:
-        for line in f:
-            line = line.strip()
-            if not line:
-                continue
-            parts = line.split("_!_")
-            if len(parts) < 4:
-                continue
-            train_texts.append(parts[3])
-            train_labels_raw.append(parts[1])
-
-    val_texts = []
-    val_labels_raw = []
-    dev_file = os.path.join(config['data_dir'], "dev_1k.txt")
-    with open(dev_file, "r", encoding="utf-8") as f:
-        for line in f:
-            line = line.strip()
-            if not line:
-                continue
-            parts = line.split("_!_")
-            if len(parts) < 4:
-                continue
-            val_texts.append(parts[3])
-            val_labels_raw.append(parts[1])
-
-
-    all_raw_labels = train_labels_raw + val_labels_raw
-    unique_labels = sorted(list(set(all_raw_labels)))
-    label_map = {raw_id: idx for idx, raw_id in enumerate(unique_labels)}
-
-    train_labels = [label_map[raw_id] for raw_id in train_labels_raw]
-    val_labels = [label_map[raw_id] for raw_id in val_labels_raw]
-
-    print(f"共有 {len(unique_labels)} 个类别")
-
     train_dataset = testDataset(
-        texts=train_texts,
-        labels=train_labels,
+        file_path=os.path.join(config['data_dir'], "train_3k.txt"),
         max_len=config['max_length']
     )
-
     dev_dataset = testDataset(
-        texts=val_texts,
-        labels=val_labels,
+        file_path=os.path.join(config['data_dir'], "dev_1k.txt"),
         max_len=config['max_length']
     )
-
+    model = berttestClassifier(num_classes=train_dataset.num_classes).to(device)
     train_loader = DataLoader(train_dataset, batch_size=config['train_batch_size'], shuffle=True)
     dev_loader = DataLoader(dev_dataset, batch_size=config['dev_batch_size'], shuffle=False)
 
-    model = berttestClassifier(num_classes=config['num_labels']).to(device)
     optimizer_grouped_parameters = [{
             "params": [p for n, p in model.named_parameters() if "bert" in n],
             "lr": 5e-6, },
